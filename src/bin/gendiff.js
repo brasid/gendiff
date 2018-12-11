1980e071cb6033b62a7bedca7bc0e1003639359c
brasid <alexanderfuf@gmail.com>
2018-12-11 09:13:17 +0300

    add arguments and description

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,8 @@ import program from 'commander';
 
 program
   .version('0.0.1')
-  .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'Output format')
   .description('Compares two configuration files and shows a difference.')
-  .program.parse(process.argv);
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>');
+
+program.parse(process.argv);
