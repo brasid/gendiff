efefcf2a82bf55ddf9955a9d908cddfd86a927f0
brasid <alexanderfuf@gmail.com>
2018-12-12 21:00:58 +0300

    refactor tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,28 +2,24 @@ import fs from 'fs';
 import genDiff from '../src';
 
 describe('GenDiff', () => {
+  const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
   it('check json', () => {
-    const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
     const processed = genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json');
     expect(processed).toBe(expected);
   });
   it('check yml', () => {
-    const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
     const processed = genDiff('./__tests__/__fixtures__/before.yml', './__tests__/__fixtures__/after.yml');
     expect(processed).toBe(expected);
   });
   it('check ini', () => {
-    const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
     const processed = genDiff('./__tests__/__fixtures__/before.ini', './__tests__/__fixtures__/after.ini');
     expect(processed).toBe(expected);
   });
   it('check json and yml', () => {
-    const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
     const processed = genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.yml');
     expect(processed).toBe(expected);
   });
   it('check yml and ini', () => {
-    const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
     const processed = genDiff('./__tests__/__fixtures__/before.ini', './__tests__/__fixtures__/after.yml');
     expect(processed).toBe(expected);
   });
