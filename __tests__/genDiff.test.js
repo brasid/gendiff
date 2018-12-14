e34c63af8a61687f26f5c5367d34317fbba78ff0
brasid <alexanderfuf@gmail.com>
2018-12-14 23:23:25 +0300

    refactoring

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -7,18 +7,21 @@ const extNames = ['.json', '.yml', '.ini'];
 
 const getExtName = (num1, num2) => extNames[getRandomNum(num1, num2)];
 
+const ext1 = getExtName(0, 2);
+const ext2 = getExtName(1, 3);
+
 test('PlainFiles', () => {
   const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
-  const before = `./__tests__/__fixtures__/before${getExtName(0, 2)}`;
-  const after = `./__tests__/__fixtures__/after${getExtName(1, 3)}`;
+  const before = `./__tests__/__fixtures__/before${ext1}`;
+  const after = `./__tests__/__fixtures__/after${ext2}`;
   const processed = genDiff(before, after);
   expect(processed).toBe(expected);
 });
 
 test('NestedFiles', () => {
   const expected = fs.readFileSync('./__tests__/__fixtures__/expectNested', 'utf-8');
-  const before = `./__tests__/__fixtures__/beforeNested${getExtName(0, 2)}`;
-  const after = `./__tests__/__fixtures__/afterNested${getExtName(1, 3)}`;
+  const before = `./__tests__/__fixtures__/beforeNested${ext1}`;
+  const after = `./__tests__/__fixtures__/afterNested${ext2}`;
   const processed = genDiff(before, after);
   expect(processed).toBe(expected);
 });
