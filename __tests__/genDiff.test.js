97ae9e018f3ac7d5bf72a5c3ef1ea280aa6be922
brasid <alexanderfuf@gmail.com>
2018-12-15 17:40:38 +0300

    add plain renderer tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -7,27 +7,38 @@ const nestedNames = ['./__tests__/__fixtures__/beforeNested', './__tests__/__fix
 
 const getCartesianFiles = (exts, names) => exts.map(ext => names.map(name => [ext, name].join('')));
 
-const getCartesianTests = (beforeArr, afterArr, expected) => beforeArr.map(
+const getCartesianTests = (beforeArr, afterArr, expected, format) => beforeArr.map(
   (before, indexBefore) => afterArr.map(
     (after, indexAfter) => it(`set${indexBefore}.${indexAfter}`, () => {
-      const actual = genDiff(before, after);
+      const actual = genDiff(before, after, format);
       expect(actual).toBe(expected);
     }),
   ),
 );
 
-describe('PlainFiles', () => {
+describe('SimpleFiles -f standart', () => {
   const plainFiles = getCartesianFiles(plainNames, extNames);
   const expected = fs.readFileSync('./__tests__/__fixtures__/expected', 'utf-8');
   const beforeArr = plainFiles[0];
   const afterArr = plainFiles[1];
-  getCartesianTests(beforeArr, afterArr, expected);
+  const format = 'standart';
+  getCartesianTests(beforeArr, afterArr, expected, format);
 });
 
-describe('NestedFiles', () => {
+describe('NestedFiles -f standart', () => {
   const nestedFiles = getCartesianFiles(nestedNames, extNames);
   const expected = fs.readFileSync('./__tests__/__fixtures__/expectNested', 'utf-8');
   const beforeArr = nestedFiles[0];
   const afterArr = nestedFiles[1];
-  getCartesianTests(beforeArr, afterArr, expected);
+  const format = 'standart';
+  getCartesianTests(beforeArr, afterArr, expected, format);
+});
+
+describe('NestedFiles -f plain', () => {
+  const nestedFiles = getCartesianFiles(nestedNames, extNames);
+  const expected = fs.readFileSync('./__tests__/__fixtures__/expectPlain', 'utf-8');
+  const beforeArr = nestedFiles[0];
+  const afterArr = nestedFiles[1];
+  const format = 'plain';
+  getCartesianTests(beforeArr, afterArr, expected, format);
 });
